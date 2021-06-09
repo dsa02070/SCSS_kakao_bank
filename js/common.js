d8a7077fc72e55d555020b9338f4e46efa0f5b42
dsa02070 <82920762+dsa02070@users.noreply.github.com>
2021-06-09 22:15:44 +0900

    gnb submenu, main_page 수정
    - gnb submenu를 ul 크기에 맞게 4등분으로 수정했습니다. 거기에서 발생된 뒷 배경 전체는  jquery에서  header영역 배경을 아래로 늘려주었습니다.
    
    - main page 이미지와 텍스트를 padding과 margin으로 위치를 잡아주었다면 width를 1080px, margin : 0 auto로 가운데 정렬한 다음 flex정렬을 한 뒤에 틀어진 부분은 padding과 margin으로 조금씩 위치를 수정했습니다.

--- js/common.js
+++ js/common.js
@@ -1,11 +1,13 @@
-$(function(){
+$(function () {
 
     // gnb, submenu hover event
-    $('.main_menu').mouseenter(function(){
+    $('.main_menu').mouseenter(function () {
         $('.submenu_wrap').addClass('on');
+        $('header').css('height', '300px');
     })
-    $('header').mouseleave(function(){
-        $('.submenu_wrap').removeClass('on')
+    $('header').mouseleave(function () {
+        $('.submenu_wrap').removeClass('on');
+        $('header').css('height', '60px');
     })
 
 })
