551f8a37466f27b2c640b5949e99f990a2241481
gitname <82920762+dsa02070@users.noreply.github.com>
2021-06-17 21:58:02 +0900

    swiper ie 대응 결과
    1. 스와이퍼 낮은 버전을 사용하여 ie11 버전에서 사용할 수 있도록 시도했으나 brand.html에서 유튜브 iframe을 사용한 스와이퍼가 불안정하게 작동합니다. 하지만 메인페이지 슬라이드는 이전 다음 버튼을 제외하고 정상적으로 작동하고 있습니다.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-$(function(){
-    //swiper script
-    let swiper = new Swiper(".kakao_card", {
-        loop : true,
-        slidesPerView: 3,
-        spaceBetween: 30,
-        centeredSlides: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-          },
-      });
+$(function () {
+  //swiper script
+  let swiper = new Swiper(".kakao_card", {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 }) //document ready
